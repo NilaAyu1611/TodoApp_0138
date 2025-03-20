5d34fd63c2977180c974b86186fcc6bdb56090b0
NilaAyu1611 <nilaayucw@gmail.com>
2025-03-20 22:13:38 +0700

    mengganti nama class(sesuai nama file)

---
+++ lib/taskform_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskFormPage extends StatefulWidget {
+  const TaskFormPage ({super.key});
+
+  @override
+  State<TaskFormPage > createState() => _TaskFormPageState();
+}
+
+class _TaskFormPageState extends State<TaskFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
